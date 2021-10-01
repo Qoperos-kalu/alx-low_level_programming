d0e1af82ae195d84f998c76e585fdf8678733301
Qoperos-kalu <kaleabmesfin399@gmail.com>
2021-10-01 02:29:35 -0700

    Task4 modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,2 @@
 #!/bin/bash
-#include<stdio.h>
-/*
-*
-*this is a code that prints using puts
-*
-*/
-int main(void)
-{
-    int puts("\"Programming is like building a multilingual puzzle");
-    return(0);
-}
+int puts(const char * "\"Programming is like building a multilingual puzzle /n")
