834adf8b2647f0ccdc0d95db3eca16e3091d3a6a
Qoperos-kalu <kaleabmesfin399@gmail.com>
2021-10-01 02:06:03 -0700

    Mandatoy task 4 modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-int puts(const char * "\"Programming is like building a multilingual puzzle /n")
+#include<stdio.h>
+/*
+*
+*this is a code that prints using puts
+*
+*/
+int main(void)
+{
+    int puts("\"Programming is like building a multilingual puzzle /n");
+    return(0);
+}
