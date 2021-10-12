68e772be08c74d5933ccba39b28cbe83151b664f
Qoperos-kalu <kaleabmesfin399@gmail.com>
2021-10-12 11:45:44 -0700

    Mandatory task 1 fucntions alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,17 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * print_alphabet - prints alphabet
+ *print_alphabet - prints lowercase alphabet
+ *prints letters from a tto z in lower case
  */
 void print_alphabet(void)
 {
-  char c;
+  char n = 'a';
 
-  for (c = 'a'; c <= 'z'; c++)
-    _putchar(c);
+  while (n <= 'z')
+    {
+      _putchar(n);
+      n++;
+    }
   _putchar('\n');
 }
