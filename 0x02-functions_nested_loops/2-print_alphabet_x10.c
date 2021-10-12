5ddf91922e2d1e220b849f41e62d1932639132bb
Qoperos-kalu <kaleabmesfin399@gmail.com>
2021-10-12 12:10:55 -0700

    mandatory task 1 functions x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints the alphabet 10x
+ */
+void print_alphabet_x10(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{   
+char n = 'a':
+while (n <= 'z')
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
+}
